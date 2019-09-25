35ff43fa378ef4df3d0f6ab9437131cec7f2bec3
Logan Gosch <logan.gosch.temp@gmail.com>
2019-09-25 09:50:15 -0500

    bruh

---
+++ src/Examples/DivideEvenly2.java
@@ -0,0 +1,20 @@
+package Examples;
+
+public class DivideEvenly2 {
+    public static void main(String[] args)
+    {
+        int limit = 100;
+
+        for(int i = 0; i < limit; ++i){
+            if(limit % i == 0)
+            System.out.println(i);
+    }
+
+    for(int j = 10; j > 0; --j){
+            System.out.print(j);
+
+    }
+
+
+    }
+}
---
+++ src/Examples/TestFusedLoopTime.java
@@ -0,0 +1,38 @@
+package Examples;
+
+import java.time.LocalDateTime;
+
+public class TestFusedLoopTime {
+    public static void main(String[] args)
+    {
+    int startTime;
+    int endTime;
+    final int REPEAT = 5_000_000;
+    final int FACTOR = 1_000_000;
+    LocalDateTime now;
+    now = LocalDateTime.now();
+    startTime = now.getNano();
+    for(int x = 0; x < REPEAT; ++x)
+        method1();
+    for(int x = 0; x < REPEAT; ++x)
+        method2();
+    now = LocalDateTime.now();
+    endTime = now.getNano();
+    System.out.println("Time for loops executed separately: " + ((endTime - startTime) / FACTOR) + " milliseconds");
+    now = LocalDateTime.now();
+    startTime = now.getNano();
+    for(int x = REPEAT;  x >= 0; --x)
+    {
+        method1();
+        method2();
+    }
+    now = LocalDateTime.now();
+    endTime = now.getNano();
+    System.out.println("Time for loops executed in a block: " + ((endTime - startTime) / FACTOR) + " milliseconds");
+
+}
+    public static void method1(){
+    }
+    public static void method2(){
+    }
+}
